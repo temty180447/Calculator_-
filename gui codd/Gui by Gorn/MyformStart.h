6d789c74f0c211ad04f1bc868261c3ff50ecc2ec
ThanakornSaokum <141802771+ThanakornSaokum@users.noreply.github.com>
2024-02-24 22:30:50 +0700

    Update MyformStart.h

--- gui codd/Gui by Gorn/MyformStart.h
+++ gui codd/Gui by Gorn/MyformStart.h
@@ -1,1 +1,142 @@
+#pragma once
+#include "MyForm.h"
+
+namespace CalculatorNew {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyFormStart
+	/// </summary>
+	public ref class MyFormStart : public System::Windows::Forms::Form
+	{
+	public:
+		MyFormStart(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyFormStart()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Panel^ panel1;
+	protected:
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Timer^ timer1;
+	private: System::ComponentModel::IContainer^ components;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->panel1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->panel2);
+			this->panel1->Location = System::Drawing::Point(0, 417);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(609, 20);
+			this->panel1->TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this->panel2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(100)), static_cast<System::Int32>(static_cast<System::Byte>(210)),
+				static_cast<System::Int32>(static_cast<System::Byte>(145)));
+			this->panel2->Location = System::Drawing::Point(0, 0);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(101, 20);
+			this->panel2->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 27.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->label1->Location = System::Drawing::Point(208, 193);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(215, 42);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"LOADING...";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->label2->Location = System::Drawing::Point(3, 389);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(272, 25);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Create By Group เฮือกสุดท้าย";
+			// 
+			// timer1
+			// 
+			this->timer1->Enabled = true;
+			this->timer1->Tick += gcnew System::EventHandler(this, &MyFormStart::timer1_Tick);
+			// 
+			// MyFormStart
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->ClientSize = System::Drawing::Size(609, 437);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->panel1);
+			this->Name = L"MyFormStart";
+			this->Text = L"MyFormStart";
+			this->panel1->ResumeLayout(false);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+		panel2->Width += 40;
+		if (panel2->Width >= 609) {
+			timer1->Stop();
+			MyForm^ myForm = gcnew MyForm(); // สร้างอ็อบเจ็กต์ของ MyForm
+			myForm->Show(); // แสดง MyForm
+			this->Hide(); // ซ่อนหน้าปัจจุบัน
+		}
+	}
+};
+}
 
